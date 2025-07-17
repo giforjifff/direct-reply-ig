dfa9bfc0f6f3aae58e12695202e6428562757ede
giforjifff <69309137+giforjifff@users.noreply.github.com>
2025-07-17 21:32:12 +0530

    reddis installed

--- services/database.js
+++ services/database.js
@@ -1,30 +1,20 @@
 // services/database.js
-import { createClient } from '@vercel/kv';
+import { createClient } from 'redis';
 
-const kv = createClient({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN,
-});
+const reddis = ()=> createClient({
+  url: process.env.KV_REST_API_URL
+}).connect();
 
-/**
- * Retrieves post details from the database using a prefixed key.
- * @param {'instagram' | 'facebook'} platform - The platform name.
- * @param {string} postId - The ID of the post.
- * @returns {Promise<object|null>} The links object or null.
- */
 export async function getPostDetails(platform, postId) {
+  kv = await reddis();
   const key = `${platform}:${postId}`;
   console.log(`Querying DB for key: ${key}`);
   return await kv.get(key);
 }
 
-/**
- * Sets the post details in the database.
- * @param {'instagram' | 'facebook'} platform - The platform name.
- * @param {string} postId - The ID of the post.
- * @param {object} links - The object containing links { amazon, flipkart }.
- */
+
 export async function setPostDetails(platform, postId, links) {
+  kv = await reddis();
   const key = `${platform}:${postId}`;
   console.log(`Setting data for key: ${key}`);
   return await kv.set(key, links);
