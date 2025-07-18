82a2401712d736845dc28e865a1d43b7ca5ee470
giforjifff <69309137+giforjifff@users.noreply.github.com>
2025-07-18 18:10:37 +0530

    moveove dhte alternative

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // index.js
 import 'dotenv/config'
 import express from 'express';
-import bodyParser from 'body-parser';
 import webhooksRouter from './routes/webhooks.js';
 import adminRouter from './routes/admin.js';
 
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json());
 
 // Set up routes
 app.use('/', webhooksRouter); // Handles /instagram
