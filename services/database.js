b1343380387042ebea1dd09c9e0d5bddf5caf398
giforjifff <69309137+giforjifff@users.noreply.github.com>
2025-07-22 15:31:55 +0530

    adding log to connection

--- services/database.js
+++ services/database.js
@@ -7,7 +7,12 @@ const redisClient = createClient({
 redisClient.on('error', err => console.error('Redis Client Error', err));
 
 (async () => {
-  await redisClient.connect();
+  try {
+    await redisClient.connect();
+    console.log('Redis connected successfully!');
+  } catch (err) {
+    console.error('FAILED TO CONNECT TO REDIS', err);
+  }
 })();
 
 
