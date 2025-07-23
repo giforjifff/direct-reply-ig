a08a928c59009e78f813bd3bdcfed35e72764590
giforjifff <69309137+giforjifff@users.noreply.github.com>
2025-07-23 13:39:11 +0530

    logging the rddis object

--- services/database.js
+++ services/database.js
@@ -12,6 +12,9 @@ redisClient.on('end', () => console.log('Redis client connection closed.'));
 
 export async function getPostDetails(platform, postId) {
   const key = `${platform}:${postId}`;
+  await redisClient.connect();
+  console.log('reddis client connected or not: ', redisClient);
+  
   console.log('key inside the getPostDetail function: ', key);
   const data = await redisClient.get(key);
   console.log('data inside the getPostDetail function: ', data);
