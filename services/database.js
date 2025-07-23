05d3fc3de878f2c1c04ac83c57b095fdc6583946
giforjifff <69309137+giforjifff@users.noreply.github.com>
2025-07-23 14:00:22 +0530

    added legacymode

--- services/database.js
+++ services/database.js
@@ -1,7 +1,8 @@
 import { createClient } from 'redis';
 
 export const redisClient = createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
+  legacyMode: true
 });
 
 redisClient.on('error', err => console.error('Redis Client Error', err));
